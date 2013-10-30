ec7522192022193065bc99d19e8e0cd2524ac5f2
MK <var@autogestion.ru>
2013-10-30 13:26:56 +0200

    set reverse relations

--- project/apps/base/api.py
+++ project/apps/base/api.py
@@ -22,9 +22,9 @@ class RoleResource(ModelResource):
     class Meta:
         queryset = RedRole.objects.all()
         resource_name = 'role'
-        # filtering = {
-        #             'id': ALL,
-        #         }
+        filtering = {
+                    'id': ALL,
+                }
         include_resource_uri = False
 
 
@@ -49,11 +49,72 @@ class RoleSetResource(ModelResource):
 
 
 
+
+class TaskResource(ModelResource):
+
+    project = fields.ForeignKey( 'project.apps.base.api.ProjectResource', 'project')
+    version = fields.ForeignKey('project.apps.base.api.VersionResource', 'version', null=True, blank=True)
+    author = fields.ForeignKey(UserResource, 'author')
+    assigned_to = fields.ForeignKey(UserResource, 'assigned_to', null=True, blank=True)
+
+    class Meta:
+        queryset = RedTask.objects.all()
+        resource_name = 'task'
+        filtering = {
+            'project': ALL_WITH_RELATIONS,
+            'version': ALL_WITH_RELATIONS,
+            'author': ALL_WITH_RELATIONS, 
+            'assigned_to': ALL_WITH_RELATIONS,             
+        }
+        include_resource_uri = False
+
+
+    def dehydrate_project(self, bundle):
+        return bundle.obj.project.id      
+
+    def dehydrate_version(self, bundle):
+        return bundle.obj.version.id   
+
+    def dehydrate_author(self, bundle):
+        return bundle.obj.author.id   
+
+    def dehydrate_assigned_to(self, bundle):
+        if bundle.obj.assigned_to:
+            return bundle.obj.assigned_to.id  
+        else:
+            return None 
+
+
+
+class VersionResource(ModelResource):
+
+    project = fields.ForeignKey('project.apps.base.api.ProjectResource', 'project')
+    tasks = fields.ToManyField(TaskResource, 'redtask_set', full=True)
+    
+    class Meta:
+        queryset = RedVersion.objects.all()
+        resource_name = 'version'
+        filtering = {
+                    'id': ALL,
+                    'project': ALL_WITH_RELATIONS,
+                    'tasks': ALL_WITH_RELATIONS,
+
+                }
+        include_resource_uri = False                
+
+
+    def dehydrate_project(self, bundle):
+        return bundle.obj.project.id   
+
+
+
 class ProjectResource(ModelResource):
 
     estimated_sum = fields.FloatField(readonly=True)
     spent_sum = fields.FloatField(readonly=True)
     roleset = fields.ToManyField(RoleSetResource, 'redroleset_set', full=True)
+    versions = fields.ToManyField(VersionResource, 'redversion_set', full=True)
+    # other_tasks = fields.ToManyField(TaskResource, 'redtask_set', full=True)
 
     class Meta:
         queryset = RedProject.objects.all()
@@ -85,56 +146,13 @@ class ProjectResource(ModelResource):
                 spent_sum += task.spent_hours
         return spent_sum
 
+  
+    # def dehydrate_other_tasks(self, bundle):        
 
-class VersionResource(ModelResource):
+    #     print bundle.data
+    #     print dir(bundle)
 
-    project = fields.ForeignKey(ProjectResource, 'project')
-    
-    class Meta:
-        queryset = RedVersion.objects.all()
-        resource_name = 'version'
-        filtering = {
-                    'id': ALL,
-                    'project': ALL_WITH_RELATIONS,
-                }
-        include_resource_uri = False                
+    #     return None
 
 
-    def dehydrate_project(self, bundle):
-        return bundle.obj.project.id      
-
-
-
-class TaskResource(ModelResource):
-
-    project = fields.ForeignKey(ProjectResource, 'project')
-    version = fields.ForeignKey(VersionResource, 'version', null=True, blank=True)
-    author = fields.ForeignKey(UserResource, 'author')
-    assigned_to = fields.ForeignKey(UserResource, 'assigned_to', null=True, blank=True)
-
-    class Meta:
-        queryset = RedTask.objects.all()
-        resource_name = 'task'
-        filtering = {
-            'project': ALL_WITH_RELATIONS,
-            'version': ALL_WITH_RELATIONS,
-            'author': ALL_WITH_RELATIONS, 
-            'assigned_to': ALL_WITH_RELATIONS,             
-        }
-        include_resource_uri = False
-
-
-    def dehydrate_project(self, bundle):
-        return bundle.obj.project.id      
-
-    def dehydrate_version(self, bundle):
-        return bundle.obj.version.id   
-
-    def dehydrate_author(self, bundle):
-        return bundle.obj.author.id   
-
-    def dehydrate_assigned_to(self, bundle):
-        if bundle.obj.assigned_to:
-            return bundle.obj.assigned_to.id  
-        else:
-            return None                 
+                
